3e1b664a9b01447913994360541d9ccb45516da5
ACER <ACER@192.168.2.4>
2018-07-25 20:59:08 +0530

    First push

---
+++ Sampleuploading/src/Test/SampleTesting.java
@@ -0,0 +1,13 @@
+package Test;
+
+import org.testng.annotations.Test;
+
+public class SampleTesting 
+{
+	@Test
+	public void uploadtest()
+	{
+		System.out.println("The bild should get upoaded successfully");
+	}
+
+}
